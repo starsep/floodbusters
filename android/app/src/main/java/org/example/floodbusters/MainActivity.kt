33ec68790e775e59107bb339de159f7965fbf842
Filip Czaplicki <github@starsep.com>
2021-09-25 01:09:46 +0200

    android: ArcGIS example

--- android/app/src/main/java/org/example/floodbusters/MainActivity.kt
+++ android/app/src/main/java/org/example/floodbusters/MainActivity.kt
@@ -1,11 +1,48 @@
 package org.example.floodbusters
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.esri.arcgisruntime.ArcGISRuntimeEnvironment
+import com.esri.arcgisruntime.mapping.ArcGISMap
+import com.esri.arcgisruntime.mapping.BasemapStyle
+import com.esri.arcgisruntime.mapping.Viewpoint
+import com.esri.arcgisruntime.mapping.view.MapView
+import org.example.floodbusters.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val activityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val mapView: MapView by lazy {
+        activityMainBinding.mapView
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activityMainBinding.root)
+        setupMap()
+    }
+
+    private fun setupMap() {
+        // TODO: remove API key from source code
+        ArcGISRuntimeEnvironment.setApiKey("AAPK656287b000314dccad848cf9cca8fde4En6PvvnSTslZpt1ysboHHuR6B3U4aUOgm46XUD04eGM8488LbFgFW7CdL3SE1jFD")
+        mapView.map = ArcGISMap(BasemapStyle.ARCGIS_TOPOGRAPHIC)
+        mapView.setViewpoint(Viewpoint(47.3774, 8.4766, 72000.0))
+    }
+
+    override fun onPause() {
+        mapView.pause()
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.resume()
+    }
+
+    override fun onDestroy() {
+        mapView.dispose()
+        super.onDestroy()
     }
 }
